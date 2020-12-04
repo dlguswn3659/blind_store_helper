55de7422a634129eba680bd8288b3403f56a76c7
hyunju <dlguswn3659@naver.com>
2020-12-05 04:41:07 +0900

    DEV: 가상환경 설정

--- blind_store_helper.py
+++ blind_store_helper.py
@@ -1,28 +1,55 @@
-def run_quickstart(file_name):
-  import io
-  import os
+# def run_quickstart(file_name):
+#     import io
+#     import os
 
-  # 구글 라이브러리 import
-  from google.cloud import vision
-  from google.cloud.vision import types
+#     # 구글 라이브러리 import
+#     from google.cloud import vision
+#     from google.cloud.vision import types
 
-  # 사용할 클라이언트 설정
-  client = vision.ImageAnnotatorClient()
+#     # 사용할 클라이언트 설정
+#     client = vision.ImageAnnotatorClient()
 
-  # 이미지 읽기
-  with io.open(file_name, 'rb') as image_file:
+#     # 이미지 읽기
+#     with io.open(file_name, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = types.Image(content=content)
+
+#     # label 뽑아냄.
+#     response = client.label_detection(image=image)
+#     labels = response.label_annotations
+
+#     print('Labels:')
+#     for label in labels:
+#         print(label.description + " = " + str(int(label.score*100)) + "%")
+
+
+# if __name__ == '__main__':
+#     run_quickstart("whale_bob.jpg")
+
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.abspath('whale_bob.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
     content = image_file.read()
 
-  image = types.Image(content=content)
+image = vision.Image(content=content)
 
-  # label 뽑아냄.
-  response = client.label_detection(image=image)
-  labels = response.label_annotations
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
 
-  print('Labels:')
-  for label in labels:
-    print(label.description + " = " + str(int(label.score*100)) + "%")
-
-
-if __name__ == '__main__' :
-  run_quickstart("whale_bob.jpg")
+print('Labels:')
+for label in labels:
+    print(label.description)
